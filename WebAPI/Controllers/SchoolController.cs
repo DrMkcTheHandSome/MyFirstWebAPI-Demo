c5682f58ff1dbfdc6a2f95edb42d6ff51c39c047
mlomio <mlomio@ITGLAP0028>
2020-10-13 21:53:56 +0800

    CRUD

--- WebAPI/Controllers/SchoolController.cs
+++ WebAPI/Controllers/SchoolController.cs
@@ -5,6 +5,7 @@ using Services.Interface;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace WebAPI.Controllers
@@ -19,16 +20,23 @@ namespace WebAPI.Controllers
             _schoolService = schoolService ?? throw new ArgumentNullException(nameof(schoolService));
         }
 
-        [Route("create")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        //// GET api/values // For Testing
+        //[HttpGet]
+        //public ActionResult<IEnumerable<string>> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        [HttpPost("", Name = "createSchool")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
         public IActionResult CreateSchool([FromBody] SchoolDTO schoolDTO)
         {
             try
             {
-              
+
                 var newSchool = _schoolService.CreateSchool(schoolDTO);
-                return CreatedAtAction(nameof(CreateSchool), newSchool);
+                return CreatedAtAction(nameof(CreateSchool), schoolDTO);
             }
             catch (Exception ex)
             {
@@ -36,16 +44,16 @@ namespace WebAPI.Controllers
             }
         }
 
-        [Route("update")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [HttpPut("", Name = "updateSchool")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
         public IActionResult UpdateSchool([FromBody] SchoolDTO schoolDTO)
         {
             try
             {
 
                 var newSchool = _schoolService.UpdateSchool(schoolDTO);
-                return CreatedAtAction(nameof(UpdateSchool), newSchool);
+                return CreatedAtAction(nameof(UpdateSchool), schoolDTO);
             }
             catch (Exception ex)
             {
@@ -53,15 +61,15 @@ namespace WebAPI.Controllers
             }
         }
 
-
-        [Route("read")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public IActionResult ReadSchool([FromBody] Guid schoolId)
+        [HttpGet("", Name = "getSchool")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public IActionResult ReadSchool([FromQuery] Guid id)
         {
             try
             {
-                var school = _schoolService.ReadSchool(schoolId);
+
+                var school = _schoolService.ReadSchool(id);
                 return Ok(school);
             }
             catch (Exception ex)
@@ -70,13 +78,15 @@ namespace WebAPI.Controllers
             }
         }
 
-        [Route("delete")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public IActionResult DeleteSchool([FromBody] Guid id)
+
+        [HttpPatch("", Name = "deleteSchool")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public IActionResult DeleteSchool([FromQuery] Guid id)
         {
             try
             {
+
                 _schoolService.DeleteSchool(id);
                 return Ok();
             }
@@ -85,5 +95,8 @@ namespace WebAPI.Controllers
                 return BadRequest(ex.ToString());
             }
         }
+
+
+
     }
 }
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -14,6 +14,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Services;
 using Services.Interface;
+using Swashbuckle.Swagger;
 
 namespace WebAPI
 {
@@ -30,12 +31,33 @@ namespace WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
-
+        
             services.AddDbContext<MyFirstWebAPIDbContext>(options => options.UseSqlServer(
                 Configuration.GetConnectionString("myFirstWebAPIConnection"))); // Database Connection String
 
             services.AddTransient<ISchoolRepository, SchoolRepository>();
             services.AddScoped<ISchoolService, SchoolService>();
+
+            services.AddSwaggerGen(setupAction =>
+            {
+                setupAction.SwaggerDoc(
+                    "MyFirstWebAPIOpenAPISpecification",
+                    new Microsoft.OpenApi.Models.OpenApiInfo()
+                    {
+                        Title = "My First Web API",
+                        Version = "1",
+                        Description = "This is my first web api.",
+                        Contact = new Microsoft.OpenApi.Models.OpenApiContact()
+                        {
+                            Email = "mlomio@blastasia.com",
+                            Name = "Marc Kenneth Lomio",
+                            Url = new Uri("https://www.linkedin.com/in/marc-kenneth-lomio-a5b646190/")
+                        }
+                    });
+            });
+
+  
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,7 +74,19 @@ namespace WebAPI
                 app.UseHsts();
             }
 
+
             app.UseHttpsRedirection();
+            // This setup is only applicable for Target framework .NET CORE 3.1 
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("MyFirstWebAPIOpenAPISpecification/swagger.json", "My API V1"); 
+            });
+
             app.UseStaticFiles();
 
             app.UseRouting();
@@ -62,6 +96,7 @@ namespace WebAPI
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
         }
     }
